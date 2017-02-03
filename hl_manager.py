fd87fc907c25cf6f7c3a05a584946e8443ada05d
Sebastian Rodriguez <srodri44@illinois.edu>
2017-02-03 15:38:18 -0600

    Communicated LLH and HLM nodes

--- hl_manager.py
+++ hl_manager.py
@@ -3,19 +3,37 @@
 import rospy
 from std_msgs.msg import String
 
+# ------------------
+# Callback Functions
+# ------------------
+
+def callback(data):
+	rospy.loginfo(rospy.get_caller_id() + ' HL: I heard %s', data.data)
+
+# -----------
+# Node Source
+# -----------
+
+# TODO: clean up and comment like ll_handler.py
+
 def hl_handler():
-	pub = rospy.Publisher('timeline', String, queue_size=10)
-	rospy.init_node('hl_handler', anonymous=True)
-	rate = rospy.Rate(5)
+	rospy.init_node('HL_manager', anonymous=False)
+	#toII_pub = 
+	toLLH_pub = rospy.Publisher('HLM_to_LLH', String, queue_size=10)
+	rospy.Subscriber('LLH_to_HLM', String, callback)
+	rate = rospy.Rate(1)
 	while not rospy.is_shutdown():
-		somestr = 'hello'
-		rospy.loginfo(somestr)
-		pub.publish(somestr)
+		histr = 'Hi from HL'
+		#rospy.loginfo(histr)
+		toLLH_pub.publish(histr)
 		rate.sleep()
+
+# -----------------------
+# Main and Error Handling
+# -----------------------
 
 if __name__ == '__main__':
 	try:
 		hl_handler()
 	except rospy.ROSInterruptException:
 		pass
-
--- ll_handler.py
+++ ll_handler.py
@@ -3,15 +3,43 @@
 import rospy
 from std_msgs.msg import String
 
+# ------------------
+# Callback Functions
+# ------------------
+
+def callback(data):
+	rospy.loginfo(rospy.get_caller_id() + ' LL: I heard %s', data.data)
+
+# -----------
+# Node Source
+# -----------
+
 def ll_handler():
-	pub = rospy.Publisher('timeline', String, queue_size=10)
-	rospy.init_node('ll_handler', anonymous=True)
-	rate = rospy.Rate(5)
+
+	# Initialize the node with False parameter to reject duplicate nodes
+	rospy.init_node('LL_handler', anonymous=False)
+
+	# Initialize Publisher topics (senders)
+	#toDC_pub = rospy.Publisher('LLH_to_DC', String, queue_size=10)
+	toHLM_pub = rospy.Publisher('LLH_to_HLM', String, queue_size=10)
+
+	# Initialize Subscriber topics (receivers)
+	rospy.Subscriber('HLM_to_LLH', String, callback)
+	#rospy.Subscriber('DC_to_LLH', String, callback)
+
+	# Initialize rate
+	rate = rospy.Rate(1)
+
+	# Run node
 	while not rospy.is_shutdown():
-		somestr = 'hello'
-		rospy.loginfo(somestr)
-		pub.publish(somestr)
+		histr = 'Hi from LL'
+		#rospy.loginfo(histr)
+		toHLM_pub.publish(histr)
 		rate.sleep()
+
+# -----------------------
+# Main and Error Handling
+# -----------------------
 
 if __name__ == '__main__':
 	try:
