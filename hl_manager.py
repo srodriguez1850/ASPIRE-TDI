4ce3b398bd027ac43421a0af35d5874086a88827
Sebastian Rodriguez <srodri44@illinois.edu>
2017-02-06 15:13:16 -0600

    Implementing databases for execution timelines

--- hl_manager.py
+++ hl_manager.py
@@ -3,6 +3,12 @@
 import rospy
 from std_msgs.msg import String
 
+# ----------------
+# Helper Functions
+# ----------------
+
+#def init_database():
+
 # ------------------
 # Callback Functions
 # ------------------
@@ -14,14 +20,27 @@ def callback(data):
 # Node Source
 # -----------
 
-# TODO: clean up and comment like ll_handler.py
-
 def hl_handler():
+
+	# Initialize the node with False parameter to reject duplicate nodes
 	rospy.init_node('HL_manager', anonymous=False)
-	#toII_pub = 
+
+	# Initialize database of High Level Tasks (Actions/Fixes/Interrupts)
+	A_db = {'bring_obj_to_user' : [0, 1, 2, 3]}
+	print A_db
+
+	# Initialize Publisher topics (senders)
+	#toII_pub = rospy.Publisher('HLM_to_II', String, queue_size=10)
 	toLLH_pub = rospy.Publisher('HLM_to_LLH', String, queue_size=10)
+
+	# Initialize Subscriber topics (receivers)
 	rospy.Subscriber('LLH_to_HLM', String, callback)
+	#rospy.Subscriber('II_to_HLM', String, callback)
+
+	# Initialize rate
 	rate = rospy.Rate(1)
+
+	# Run node
 	while not rospy.is_shutdown():
 		histr = 'Hi from HL'
 		#rospy.loginfo(histr)
