522aefcd6fde1f6c69c01a33f1c32a2b79fb59c8
ChuAmberZhang <czhng100@illinois.edu>
2017-04-12 13:45:02 -0500

    Added simple GUI example for client

--- client/client.py
+++ client/client.py
@@ -1,8 +1,30 @@
 import sys, socket, select
+try: 
+    # for Python2
+    from Tkinter import Tk, Label, Button
+except ImportError:
+    # for Python3
+    from tkinter import Tk, Label, Button
+
+class MyFirstGUI:
+    def __init__(self, master, server_socket):
+        self.master = master
+        self.server = server_socket
+        master.title("GUI")
+
+        self.label = Label(master, text="Click to send command!")
+        self.label.pack()
+
+        self.greet_button = Button(master, text="Send Command", command=self.sendCommand)
+        self.greet_button.pack()
+
+    def sendCommand(self):
+        self.server.send(b"bring_obj_to_user")
+
  
 def client():
     if(len(sys.argv) < 3) :
-        print 'Usage : python client.py host port'
+        print('Usage : python client.py host port')
         sys.exit()
     host = sys.argv[1]
     port = int(sys.argv[2])
@@ -13,10 +35,14 @@ def client():
     try :
         server_socket.connect((host, port))
     except :
-        print 'Failed to connect :('
+        print('Failed to connect :(')
         sys.exit()
      
-    print 'Connected :)'
+    print('Connected :)')
+
+    root = Tk()
+    my_gui = MyFirstGUI(root, server_socket)
+    root.mainloop()
      
     socket_list = [sys.stdin, server_socket]
     while True:
@@ -33,13 +59,13 @@ def client():
                     sys.stdout.write(mesg) 
                 else :
                     # disconnected
-                    print '\nDisconnected :('
+                    print('\nDisconnected :(')
                     sys.exit()
             
             else :
                 # send message
                 mesg = sys.stdin.readline()
-                server_socket.send(mesg)  
+                server_socket.send(mesg.encode())  
                 
 
 if __name__ == "__main__":
